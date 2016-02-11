f4d9640805c741698c96461e2aed10332621f4bb
cremones <francesco.cremonesi@epfl.ch>
2016-02-11 21:16:00 +0100

    drone update

--- src/prelim/prelim.cpp
+++ src/prelim/prelim.cpp
@@ -100,9 +100,11 @@ void prelim::output_prelim_data (std::string outfile) {
 }
 
 
-void preim::deliver() {
+void prelim::deliver() {
     for (int t = 0; t < nturns_; t++) {
-        std::foreach(drones.begin(), drones.end(), drone::update() );
+        for( std::vector<drone>::iterator it = drones.begin(), it != drones.end(), it++ ) {
+           update_drone(*it);
+        }
 
         while (orders_left and avail_drones) {
             order cur_ord = choose_order();
@@ -126,10 +128,29 @@ void prelim::put_order (order & cur_order) {
     update_order();
 }
 
-void drone::update() {
+void prelim::update_drone(drone& d) {
 
-    
-
+    if ( d.turns_left_) {
+        d.turns_left_--;
+    } else if ( !d.immediate_pending_commands_.is_empty() ) {
+        command todo = d.immediate_pending_commands_.pop();
+        switch(todo.type_) {
+            case LOAD:
+                warehouses[todo.wid_] -= nitems_;
+                d.load[todo.pid_] += nitems_;
+                break;
+            case UNLOAD:
+                d.load[todo.pid_] -= nitems_;
+                orders[todo.oid_].delv_[todo.pid_] += nitems_; 
+                break;
+            case MOVE:
+                d.turns_left_ += pid_;
+                break;
+            default:
+                std::cerr << "Unrecognized command" << std::endl; 
+                exit(1);
+        }
+    }    
 }
 
 
--- src/prelim/prelim.h
+++ src/prelim/prelim.h
@@ -11,12 +11,15 @@
 #include <cstddef>
 #include <cstdlib>
 #include <deque>
-enum command_type {LOAD, UNLOAD};
+class prelim;
+
+enum command_type {LOAD, UNLOAD, MOVE};
 
 struct command{
 	int did_;
 	command_type type_;
 	int wid_;
+	int oid_;
 	int pid_;
 	int nitems_;
 	void print(std::ostream &out) const;
@@ -49,14 +52,19 @@ struct drone {
 
 	void add_command();
 
-        void update();
-        
 };
 
 struct order {
 	coord pos_;
 	int nitems_;
 	std::vector<int> req_;
+        std::vector<int> delv_;
+
+        bool is_fulfilled() {
+            return
+            std::accumulate(req_.begin() , req_end()) == 0 &&
+            std::accumulate(delv_.begin(), delv_.end()) == nitems_;
+        }
 };
 
 class prelim {
@@ -81,6 +89,8 @@ public:
 	void print(std::ostream &out) const;
 
 	void output_prelim_data(std::string outfile);
+
+        void update_drone(drone & );
 };
 
 std::ostream &operator<<(std::ostream &out, const prelim &p);
